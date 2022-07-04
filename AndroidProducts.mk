01fd0b7a56811c780c6bd22da730c0d1fbee586d
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2022-07-04 12:21:00 -0400

    pocket2plus: Switch to COMMON_LUNCH_CHOICES * add_lunch_combo is obsolete. Use COMMON_LUNCH_CHOICES in your AndroidProducts.mk instead

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_pocket2plus.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_pocket2plus-eng \
+    lineage_pocket2plus-userdebug \
+    lineage_pocket2plus-user
--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-# Add pocket2plus to lunch
-add_lunch_combo lineage_pocket2plus-eng
-add_lunch_combo lineage_pocket2plus-userdebug
-add_lunch_combo lineage_pocket2plus-user
