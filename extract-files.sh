fff97ba9300ccf17d6d0b1d6c35ff8ea74549163
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2022-12-29 19:56:49 -0500

    pocket2plus: Update proprietary files extracting scripts * Correct path to extract_utils.sh and remove blob fixup for now

--- extract-files.sh
+++ extract-files.sh
@@ -12,7 +12,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 LINEAGE_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${LINEAGE_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -39,139 +39,6 @@ done
 if [ -z "$SRC" ]; then
     SRC=adb
 fi
-
-function blob_fixup() {
-    case "${1}" in
-        vendor/lib/hw/camera.ums312.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-        vendor/lib64/hw/camera.ums312.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-        vendor/lib/libcamdrv.so)
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-        vendor/lib64/libcamdrv.so)
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-        vendor/lib/libcamoem.so)
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-        vendor/lib64/libcamoem.so)
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-        vendor/lib/libispalg.so)
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-        vendor/lib64/libispalg.so)
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-        vendor/lib/hw/hwcomposer.ums312.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/hw/hwcomposer.ums312.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libWT.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libWT.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libbokeh_depth.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libbokeh_depth.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libstagefright_sprd_deintl.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libstagefright_sprd_h264dec.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libstagefright_sprd_h264enc.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libstagefright_sprd_h265dec.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libstagefright_sprd_h265enc.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libstagefright_sprd_mpeg4dec.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libstagefright_sprd_mpeg4enc.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libstagefright_sprd_vp9dec.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libstagefright_sprd_vpxdec.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libtdnsTest.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libstagefright_sprd_deintl.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libstagefright_sprd_h264dec.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libstagefright_sprd_h264enc.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libstagefright_sprd_h265dec.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libstagefright_sprd_h265enc.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libstagefright_sprd_mpeg4dec.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libstagefright_sprd_mpeg4enc.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libstagefright_sprd_vp9dec.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libstagefright_sprd_vpxdec.so)
-            ./patchelf-0_9 --add-needed "libui-v28.so" "${2}"
-            ;;
-        vendor/lib64/libtdnsTest.so)
-            ./patchelf-0_9 --replace-needed "libui.so" "libui-v28.so" "${2}"
-            ;;
-        vendor/lib/libsprdaudiohal.so)
-            ./patchelf-0_9 --replace-needed "android.hardware.audio.common@4.0-util.so" "android.hardware.audio.common@4.0-util-v28.so" "${2}"
-            ;;
-        vendor/lib/libsprdaudiohal@4.0.so)
-            ./patchelf-0_9 --replace-needed "android.hardware.audio.common@4.0-util.so" "android.hardware.audio.common@4.0-util-v28.so" "${2}"
-            ;;
-        vendor/lib64/libsprdaudiohal.so)
-            ./patchelf-0_9 --replace-needed "android.hardware.audio.common@4.0-util.so" "android.hardware.audio.common@4.0-util-v28.so" "${2}"
-            ;;
-        vendor/lib64/libsprdaudiohal@4.0.so)
-            ./patchelf-0_9 --replace-needed "android.hardware.audio.common@4.0-util.so" "android.hardware.audio.common@4.0-util-v28.so" "${2}"
-            ;;
-        vendor/lib/hw/android.hardware.audio@4.0-impl.so)
-            ./patchelf-0_9 --add-needed "android.hardware.audio.common@4.0-util-v28.so" "${2}"
-            ;;
-        vendor/lib64/hw/android.hardware.audio@4.0-impl.so)
-            ./patchelf-0_9 --add-needed "android.hardware.audio.common@4.0-util-v28.so" "${2}"
-            ;;
-        vendor/lib/hw/audio.primary.ums312.so)
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-        vendor/lib64/hw/audio.primary.ums312.so)
-            ./patchelf-0_9 --add-needed "libprocessgroup.so" "${2}"
-            ;;
-    esac
-}
 
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
