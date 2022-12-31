c1adb1efea2d1fc47ba2d82ac60cd6a48b8d09b9
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2022-12-30 20:28:02 -0500

    hidl: Switch to AOSP thermal service

--- hidl.mk
+++ hidl.mk
@@ -55,6 +55,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.mock
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
