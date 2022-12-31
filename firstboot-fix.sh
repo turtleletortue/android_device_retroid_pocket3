d89b50498e8752c59d36ffe55022567e625c8512
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2022-12-31 08:31:30 -0500

    firstboot-fix: Remove firstboot-fix.sh * This workaround is not needed anymore with the Android 11 base

--- firstboot-fix.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-fileexists=1
-
-[ ! -e /data/vendor/local/media/mmi.audio.ctrl ] && fileexists=0
-
-while [ $fileexists = 0 ]; do
-    [ -e /data/vendor/local/media/mmi.audio.ctrl ] && [ $fileexists = 0 ] && reboot
-done
