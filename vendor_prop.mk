c67e88ec0b46b3d7c6bbfe9ec3e35946ad2d57b5
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2023-01-01 22:51:19 -0500

    vendor_prop: Allow rotating the lock screen by default

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,2 +1,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=266
+
+# Force Lock Screen Rotation
+PRODUCT_PRODUCT_PROPERTIES += \
+    log.tag.launcher_force_rotate=VERBOSE \
+    lockscreen.rot_override=true
